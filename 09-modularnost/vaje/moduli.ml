99ff672c2adb5168fdff3237ac456220b5afc4d7
Ana Šenica <ana.senica@student.fmf.uni-lj.si>
2019-12-02 11:58:16 +0100

    9.vaje

--- 09-modularnost/vaje/moduli.ml
+++ 09-modularnost/vaje/moduli.ml
@@ -53,9 +53,14 @@ module type NAT = sig
 
   val eq   : t -> t -> bool
   val zero : t
-  (* Dodajte manjkajoče! *)
-  (* val to_int : t -> int *)
-  (* val of_int : int -> t *)
+  val one : t
+
+  val add : t -> t -> t
+  val substract : t -> t -> t
+  val multiply : t -> t -> t
+
+  val to_int : t -> int
+  val of_int : int -> t 
 end
 
 (*----------------------------------------------------------------------------*]
@@ -67,14 +72,51 @@ end
  [failwith "later"], vendar to ne deluje za konstante.
 [*----------------------------------------------------------------------------*)
 
+
+(* Tu definiramo konkreten modul, ki za osnovo uporablja naš NAT. NAT, ki smo ga definirali zgoraj, je osnova. *)
+
+(* TAKOLE NAPIŠEMO TISTI failwith:
 module Nat_int : NAT = struct
 
   type t = int
-  let eq x y = failwith "later"
+
+  let eq x y = x = y
   let zero = 0
-  (* Dodajte manjkajoče! *)
+  let one = 1
+
+  let add x y = failwith "later"
+  let substract x y = failwith "later"
+  let multiply x y = failwith "later"
+
+  let to_int x = x
+  let of_int x =  *)
+
+module Nat_int : NAT = struct
+
+  type t = int
+
+  let eq x y = x = y
+  let zero = 0
+  let one = 1
+
+  let add x y = x + y
+  let substract x y = if x - y < 0 then 0 else x - y
+  let multiply x y = x * y
+
+  let to_int x = x
+  let of_int x = x
 
 end
+
+(* Ko tole  vtipkamo v terminal, bo vrgel ven neko abstraktno definicijo.
+
+# Nat_int.zero;;
+- : Nat_int.t = <abstr>
+
+# Nat_int.to_int(Nat_int.zero);;
+- : int = 0
+*)
+
 
 (*----------------------------------------------------------------------------*]
  Napišite implementacijo [NAT], ki temelji na Peanovih aksiomih:
@@ -82,7 +124,7 @@ end
    
  Osnovni tip modula definirajte kot vsotni tip, ki vsebuje konstruktor za ničlo
  in konstruktor za naslednika nekega naravnega števila.
- Večino funkcij lahko implementirate s pomočjo rekurzije. Naprimer, enakost
+ Večino funkcij lahko implementirate s pomočjo rekurzije. Na primer, enakost
  števil [k] in [l] določimo s hkratno rekurzijo na [k] in [l], kjer je osnoven
  primer [Zero = Zero].
 
@@ -90,12 +132,49 @@ end
 
 module Nat_peano : NAT = struct
 
-  type t = unit (* To morate spremeniti! *)
-  let eq x y = failwith "later"
-  let zero = () (* To morate spremeniti! *)
-  (* Dodajte manjkajoče! *)
+  type t = Z | S of t (*Tu pomeni Z Zero in S Successor*)
+
+  let eq x y = x = y
+  let zero = Z
+  let one = S Z
+  
+  let rec add x y =
+    match x with
+    | Z -> y
+    | S x' -> S (add x' y)
+  
+  let rec multiply x y =
+    match y with
+    | Z -> Z
+    | S y' -> add x (multiply x y')
+
+  let rec substract x y =
+    match x, y with
+    | _, Z -> x
+    | S x, S y -> substract x y
+    | Z, _ -> Z 
+
+  let rec to_int x =
+    match x with
+    | Z -> 0
+    | S x' -> 1 + (to_int x')
+
+  let rec of_int x = if x <= 0 then Z else S (of_int (x - 1))
 
 end
+
+(* 3 ~ S(S(S Z)) *)
+
+let three = Nat_peano.of_int 3
+let seven = Nat_peano.of_int 7
+
+(*
+# Nat_peano.substract seven three;;
+- : Nat_peano.t = <abstr>
+# Nat_peano.to_int(Nat_peano.substract seven three);; 
+- : int = 4
+*)
+
 
 (*----------------------------------------------------------------------------*]
  V OCamlu lahko module podajamo kot argumente funkcij, z uporabo besede
@@ -118,7 +197,26 @@ end
  - : int = 4950
 [*----------------------------------------------------------------------------*)
 
-let sum_nat_100 (module Nat : NAT) = ()
+(* Zgoraj v testnih primerih so sešteli od 0 do vključno 99, zato pride za 100 manj kot v naši funkciji*)
+
+let sum_nat_100 (module Nat : NAT) =
+  let hundred = Nat.of_int 100 in
+  let rec sum_x_100 x =
+    if Nat.eq x hundred then 
+      hundred
+    else
+      (* V int bi to naredili tako: x + sum_x_100 (x + 1) *)
+      Nat.add x (sum_x_100 (Nat.add x Nat.one))
+  in
+  sum_x_100 Nat.zero |> Nat.to_int
+
+
+(*
+# sum_nat_100 (module Nat_peano);; 
+- : int = 5050
+# sum_nat_100 (module Nat_int);;    
+- : int = 5050
+ *)
 
 (*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*]
  Now we follow the fable told by John Reynolds in the introduction.
@@ -132,8 +230,17 @@ let sum_nat_100 (module Nat : NAT) = ()
 
 module type COMPLEX = sig
   type t
+
   val eq : t -> t -> bool
-  (* Dodajte manjkajoče! *)
+
+  val zero : t
+  val one : t
+  val i : t
+
+  val negacija : t -> t
+  val konjugacija : t -> t
+  val sestevanje : t -> t -> t
+  val mnozenje : t -> t -> t
 end
 
 (*----------------------------------------------------------------------------*]
@@ -145,7 +252,16 @@ module Cartesian : COMPLEX = struct
 
   type t = {re : float; im : float}
 
-  let eq x y = failwith "later"
+  let eq x y = x = y
+
+  let zero = {re = 0. ; im = 0.}
+  let one = {re = 1. ; im = 0.}
+  let i = {re = 0. ; im = 1.}
+  
+  let negacija x = {re = -. x.re ; im = -. x.im }
+  let konjugacija x = {re = x.re ; im = -. x.im }
+  let sestevanje x y = {re = x.re +. y.re ; im = x.im +. y.im }
+  let mnozenje x y = {re = (x.re *. y.re -. x.im *. y.im) ; im = (x.re *. y.im +. x.im *. y.re)}
   (* Dodajte manjkajoče! *)
 
 end
@@ -167,9 +283,17 @@ module Polar : COMPLEX = struct
   let rad_of_deg deg = (deg /. 180.) *. pi
   let deg_of_rad rad = (rad /. pi) *. 180.
 
-  let eq x y = failwith "later"
-  (* Dodajte manjkajoče! *)
+  let eq x y = 
+    (x.magn = 0. && y.magn = 0.)
+    || (x.magn = y.magn && x.arg = y.arg)
 
+  let zero = {magn = 0.; arg = 0.}
+  let one = {magn = 1.; arg = 0.}
+  let i = {magn = 1.; arg = pi/.2}
+
+  let negacija x = {magn = x.magn ; arg = x.arg +. pi}
+  let konjugacija x = {magn = x.magn ; arg = -. x.arg}
+  let sestevanje x y =
 end
 
 (*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*]
@@ -197,5 +321,6 @@ end
  n : 2
  - : unit = ()
 [*----------------------------------------------------------------------------*)
-
+;;
+(*Tu na konec damo ;; da s ene sesuje zaradi tele spodnje funkcije, ko želimo preverjati rešitve*)
 let count (module Dict : DICT) list = ()
